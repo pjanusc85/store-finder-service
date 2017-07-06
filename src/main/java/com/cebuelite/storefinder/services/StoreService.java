1e525337ba71146c17930f3c974007a450c38308
Paul Janus Calinawan <paul@pixalate.com>
2017-07-06 15:44:30 +0800

    added csv data

--- src/main/java/com/cebuelite/storefinder/services/StoreService.java
+++ src/main/java/com/cebuelite/storefinder/services/StoreService.java
@@ -30,6 +30,7 @@ public class StoreService {
         Page<StoreEntity> allEntities = this.repository.findAll(pageRequest);
 
         List<StoreEntity> allEntitiesContent = new ArrayList<>();
+
         StoreEntity e = new StoreEntity();
         e.setId(1L);
         e.setThumbnailUrl("https://geo1.ggpht.com/cbk?panoid=wBn05OfIlQeqKASUQpZq5w&output=thumbnail&cb_client=search.TACTILE.gps&thumb=2&w=408&h=200&yaw=160.78165&pitch=0&thumbfov=100");
@@ -40,6 +41,39 @@ public class StoreService {
         e.setContactNumber("(032) 268-3206");
         e.setContactPerson("Billie Duterte");
         allEntitiesContent.add(e);
+
+        StoreEntity e2 = new StoreEntity();
+        e2.setId(2L);
+        e2.setThumbnailUrl("https://geo1.ggpht.com/cbk?panoid=wBn05OfIlQeqKASUQpZq5w&output=thumbnail&cb_client=search.TACTILE.gps&thumb=2&w=408&h=200&yaw=160.78165&pitch=0&thumbfov=100");
+        e2.setName("Insular Life");
+        e2.setLatitude(10.320248f);
+        e2.setLongitude(123.906024f);
+        e2.setFullAddressDescription("Suite 1204, 12th Floor, Insular Life Business Centre, Biliran Road, Cebu Business Park, Cebu City, 6000 Cebu");
+        e2.setContactNumber("(032) 268-3206");
+        e2.setContactPerson("Billie Duterte");
+        allEntitiesContent.add(e2);
+
+        StoreEntity e3 = new StoreEntity();
+        e2.setId(3L);
+        e2.setThumbnailUrl("https://geo1.ggpht.com/cbk?panoid=wBn05OfIlQeqKASUQpZq5w&output=thumbnail&cb_client=search.TACTILE.gps&thumb=2&w=408&h=200&yaw=160.78165&pitch=0&thumbfov=100");
+        e2.setName("Insular Life");
+        e2.setLatitude(10.320248f);
+        e2.setLongitude(123.906024f);
+        e2.setFullAddressDescription("Suite 1204, 12th Floor, Insular Life Business Centre, Biliran Road, Cebu Business Park, Cebu City, 6000 Cebu");
+        e2.setContactNumber("(032) 268-3206");
+        e2.setContactPerson("Billie Duterte");
+        allEntitiesContent.add(e3);
+
+        StoreEntity e4 = new StoreEntity();
+        e2.setId(4L);
+        e2.setThumbnailUrl("https://geo1.ggpht.com/cbk?panoid=wBn05OfIlQeqKASUQpZq5w&output=thumbnail&cb_client=search.TACTILE.gps&thumb=2&w=408&h=200&yaw=160.78165&pitch=0&thumbfov=100");
+        e2.setName("Insular Life");
+        e2.setLatitude(10.320248f);
+        e2.setLongitude(123.906024f);
+        e2.setFullAddressDescription("Suite 1204, 12th Floor, Insular Life Business Centre, Biliran Road, Cebu Business Park, Cebu City, 6000 Cebu");
+        e2.setContactNumber("(032) 268-3206");
+        e2.setContactPerson("Billie Duterte");
+        allEntitiesContent.add(e4);
 
         List<Store> allStores = allEntitiesContent.stream().map(entity -> new Store(
                 entity.getId(),
